1197951bd7650fec95e54a486e5db08b083bab57
david-diep <ddkotan@gmail.com>
2020-07-24 00:22:36 -0700

    view products front end

---
+++ client/components/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="pl-3 bg-dark row">
+        <h2 className="m-3 text-light">
+          <i className="fas fa-comment-dollar"></i>
+          <a className="text-decoration-none text-reset" href="#">Wicked <i className="fa fa-dollar-sign" aria-hidden="true"></i>ales</a>
+        </h2>
+      </header>);
+  }
+}
+
+export default Header;
---
+++ client/components/product-list.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch('/api/products').then(res => res.json()
+    ).then(products => this.setState({ products: products }));
+  }
+
+  render() {
+    return (<>
+      <div className="d-flex flex-wrap justify-content-center">
+        {this.state.products.map(item => (
+          <ProductListItem key={item.productId} item={item} />
+        )
+        )}
+      </div>
+
+    </>
+    );
+  }
+}
+
+export default ProductList;
